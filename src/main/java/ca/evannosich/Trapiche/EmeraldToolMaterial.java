d68c72e87c51f2f286e1ab21887e6066496d5881
enosich <evannosich@icloud.com>
2021-02-07 23:14:40 -0500

    v1.1
    Temporarily rebalanced tools while v2.0 is indev

--- src/main/java/ca/evannosich/Trapiche/EmeraldToolMaterial.java
+++ src/main/java/ca/evannosich/Trapiche/EmeraldToolMaterial.java
@@ -7,29 +7,29 @@ import net.minecraft.recipe.Ingredient;
 public class EmeraldToolMaterial implements ToolMaterial {
 	@Override
 	public int getDurability() {
-		return 1796;
+		return 1024;
 	}
-	
+
 	@Override
 	public int getMiningLevel() {
 		return 3;
 	}
-	
+
 	@Override
 	public float getMiningSpeedMultiplier() {
 		return 8.0F;
 	}
-	
+
 	@Override
 	public float getAttackDamage() {
 		return 0F;
 	}
-	
+
 	@Override
 	public int getEnchantability() {
 		return 22;
 	}
-	
+
 	@Override
 	public Ingredient getRepairIngredient() {
 		return Ingredient.ofItems(Items.EMERALD);
